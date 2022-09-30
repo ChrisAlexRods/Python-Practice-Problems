34032fdbaaf2209ab2e2cedcf001a7acb6050830
Christian Rodriguez <105233007+ChrisAlexRods@users.noreply.github.com>
2022-09-30 12:38:22 -0400

    001firsttry

--- problems/problem_001.py
+++ problems/problem_001.py
@@ -9,4 +9,26 @@
 # problem to get a good feel for how to solve it.
 
 def minimum_value(value1, value2):
-    pass
+    if value1 < value2:
+        return value1
+    if value2 < value1:
+        return value2
+    elif value1 == value2:
+        return value1
+
+
+"""
+or
+if value <= value2:
+    return value1
+if value2 < value1:
+    return value1
+return value 1
+"""
+
+
+
+#test code
+print(minimum_value(1,2)) # should print 1
+print(minimum_value(3000,4000)) #should also print 1
+print(minimum_value(1,1)) # should also print 1
